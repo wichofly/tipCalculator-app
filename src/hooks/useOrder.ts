4e27567e43f6b404e72b0660d6607d7286c25f7a
Mauricio Erazo <antonierazo13@hotmail.com>
2024-10-01 22:56:33 +0200

    feat: avoided duplications and added quantities

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,12 +5,21 @@ export const useOrder = () => {
   const [order, setOrder] = useState<OrderItem[]>([]);
 
   const addItem = (item: MenuItem) => {
-    const newItem = { ...item, quantity: 1 };
-    setOrder([...order, newItem]);
+    const itemExist = order.find((orderItem) => orderItem.id === item.id);
+    if (itemExist) {
+      const updatedOrder = order.map((orderItem) =>
+        orderItem.id === item.id
+          ? { ...orderItem, quantity: orderItem.quantity + 1 }
+          : orderItem
+      );
+      setOrder(updatedOrder);
+    } else {
+      const newItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
+    }
   };
 
   console.log(order);
-  
 
   return { addItem };
 };
