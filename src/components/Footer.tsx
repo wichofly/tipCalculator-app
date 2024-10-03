3614b06653142d134cb8b4169364957c9dd47696
Mauricio Erazo <antonierazo13@hotmail.com>
2024-10-03 19:58:27 +0200

    fix: delete extra "noopener"

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         {/* Left Side - Copyright */}
         <div>
           <p className="text-2xl">
-           Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+            Copyright &copy; {new Date().getFullYear()}. All rights reserved.
           </p>
         </div>
 
@@ -18,7 +18,7 @@ const Footer = () => {
             <a
               href="https://www.linkedin.com/in/mauricio-erazo-/"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="noopener"
               className="hover:text-blue-400 transition duration-300"
               aria-label="LinkedIn"
             >
@@ -28,7 +28,7 @@ const Footer = () => {
             <a
               href="https://github.com/wichofly/tipCalculator-app"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="noopener"
               className="hover:text-gray-400 transition duration-300"
               aria-label="GitHub"
             >
@@ -38,7 +38,7 @@ const Footer = () => {
             <a
               href="https://wichofly.github.io/portfolio-website/"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="noopener"
               className="hover:text-teal-400 transition duration-300"
               aria-label="Portfolio"
             >
